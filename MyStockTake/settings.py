5261814b5fcd50aee2ba0f61f987be1f2af81ac5
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-08-21 14:35:23 +0100

    Add heroku app to trusted applications url for deployment

--- MyStockTake/settings.py
+++ MyStockTake/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    os.environ.get('TRUSTED_HOST'), os.environ.get('LOCAL_HOST')]
+    os.environ.get('TRUSTED_HOST'), os.environ.get('LOCAL_HOST')
+    ]
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
