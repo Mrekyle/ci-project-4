3b0ee82848216c9a41b8977b0f04d9d1618821e8
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-09-19 17:55:55 +0100

    Add code blocks and docstrings

--- static/js/email.js
+++ static/js/email.js
@@ -1,4 +1,6 @@
-
+/**
+ * Sending the contact form email response to the site admin using the emailJS service
+ */
 function sendMail (contactForm) {
     emailjs.send("service_g7iucrk","template_89y5wtg", {
         'from_name': contactForm.name.value, 
@@ -15,6 +17,8 @@ function sendMail (contactForm) {
     return false;
 }
 
+// Depending on the success of the email this displays a success message and will reset the form 
+
 let emailSuccess = document.getElementById('email-success');
 let resetForm = document.getElementById('contactForm');
 
@@ -23,3 +27,4 @@ resetForm.addEventListener('submit', (e) => {
     resetForm.reset();
     emailSuccess.innerHTML += 'Email submitted successfully! <br> We will reply as soon as we can.';
 });
+
