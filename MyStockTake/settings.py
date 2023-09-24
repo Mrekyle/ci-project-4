4f66ca2a35e76813721efaabe0cf0e358e940b8c
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-09-24 17:32:31 +0100

    Update settings.py

--- MyStockTake/settings.py
+++ MyStockTake/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('TRUSTED_HOST')
+ALLOWED_HOSTS = os.environ.get('.herokuapp.com')
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
